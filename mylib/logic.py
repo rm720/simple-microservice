ef96025090177649907809af8c9486b3f19713a1
Ra <ratmir.mugattarov.42@gmail.com>
2025-02-24 14:59:44 +1100

    cli

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,8 @@ def wiki(name="War Goddess", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+def search_wiki(name="War Goddess"):
+    """Seach wikipedia for names"""
+    result = wikipedia.search(name)
+    return result
